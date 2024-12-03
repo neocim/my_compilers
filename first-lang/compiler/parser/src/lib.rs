57707af77c198db380217cc2bf3c0b6b75068367
Nnenty <frag1337n@gmail.com>
2024-12-03 23:40:30 +0300

    Add `parser` module with basic expr-s

---
+++ first-lang/compiler/parser/src/lib.rs
@@ -0,0 +1,131 @@
+#[derive(Debug, Clone, PartialEq)]
+pub enum Expr {
+    LiteralExpr(LiteralExpr),
+    BinaryExpr(BinaryExpr),
+    VariableExpr(VariableExpr),
+    CallExpr(CallExpr),
+    FuncPrototype(FuncPrototype),
+    Func(Func),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct LiteralExpr {
+    kind: LiteralExprKind,
+}
+
+impl LiteralExpr {
+    pub fn new(kind: LiteralExprKind) -> Self {
+        LiteralExpr { kind }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum LiteralExprKind {
+    IntExpr(IntExpr),
+    FloatExpr(FloatExpr),
+    CharExpr(CharExpr),
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct IntExpr {
+    val: i32,
+}
+
+impl IntExpr {
+    pub fn new(val: i32) -> Self {
+        IntExpr { val }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct FloatExpr {
+    val: f32,
+}
+
+impl FloatExpr {
+    pub fn new(val: f32) -> Self {
+        FloatExpr { val }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct CharExpr {
+    val: char,
+}
+
+impl CharExpr {
+    pub fn new(val: char) -> Self {
+        CharExpr { val }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct BinaryExpr {
+    kind: BinaryExprKind,
+    left: LiteralExpr,
+    right: LiteralExpr,
+}
+
+impl BinaryExpr {
+    pub fn new(kind: BinaryExprKind, left: LiteralExpr, right: LiteralExpr) -> Self {
+        BinaryExpr { kind, left, right }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum BinaryExprKind {
+    Plus,
+    Minus,
+    Star,
+    Slash,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct VariableExpr {
+    name: String,
+}
+
+impl VariableExpr {
+    pub fn new(name: String) -> Self {
+        VariableExpr { name }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct CallExpr {
+    call: String,
+    args: Vec<Expr>,
+}
+
+impl CallExpr {
+    pub fn new(call: String, args: Vec<Expr>) -> Self {
+        CallExpr { call, args }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct FuncPrototype {
+    name: String,
+    args: Vec<String>,
+}
+
+impl FuncPrototype {
+    pub fn new(name: String, args: Vec<String>) -> Self {
+        FuncPrototype { name, args }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Func {
+    prototype: FuncPrototype,
+    body: Box<Expr>,
+}
+
+impl Func {
+    pub fn new(prototype: FuncPrototype, body: Expr) -> Self {
+        Func {
+            prototype,
+            body: Box::new(body),
+        }
+    }
+}
