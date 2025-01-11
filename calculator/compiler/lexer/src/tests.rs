893ac9589111cfb02f91797f3cb5361e7cec68e2
Nnenty <frag1337n@gmail.com>
2025-01-11 16:59:42 +0300

    Add `Lexer` tests

--- calculator/compiler/lexer/src/tests.rs
+++ calculator/compiler/lexer/src/tests.rs
@@ -1,12 +1,53 @@
 #![cfg(test)]
 
-use crate::{token::Token, Lexer};
+use crate::{
+    token::{LiteralKind, Token, TokenKind},
+    Lexer,
+};
 
 #[test]
 fn lexer_tests() {
-    let input = "123.4 * 5 + (6789.10 - 11.12131415)";
+    let input = "123.4 * 5 + (6789.12345 - 600)";
     let mut lexer = Lexer::new(input);
     let token_stream: Vec<Token> = lexer.tokenize().collect();
 
-    assert_eq!(token_stream, Vec::new())
+    // Comments for easier debugging
+    assert_eq!(
+        token_stream,
+        [
+            // `123.4 * 5 + `
+            Token::new(TokenKind::Lit {
+                kind: LiteralKind::Float {
+                    val: "123.4".to_string()
+                }
+            }),
+            Token::new(TokenKind::Whitespace),
+            Token::new(TokenKind::Star),
+            Token::new(TokenKind::Whitespace),
+            Token::new(TokenKind::Lit {
+                kind: LiteralKind::Int {
+                    val: "5".to_string()
+                }
+            }),
+            Token::new(TokenKind::Whitespace),
+            Token::new(TokenKind::Plus),
+            Token::new(TokenKind::Whitespace),
+            // `(6789.12345 - 600)`
+            Token::new(TokenKind::OpenParen),
+            Token::new(TokenKind::Lit {
+                kind: LiteralKind::Float {
+                    val: "6789.12345".to_string()
+                }
+            }),
+            Token::new(TokenKind::Whitespace),
+            Token::new(TokenKind::Minus),
+            Token::new(TokenKind::Whitespace),
+            Token::new(TokenKind::Lit {
+                kind: LiteralKind::Int {
+                    val: "600".to_string()
+                }
+            }),
+            Token::new(TokenKind::CloseParen),
+        ]
+    )
 }
