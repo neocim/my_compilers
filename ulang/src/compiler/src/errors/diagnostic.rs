99240bef14dddc6547db1010eab2afd9337d623d
neocim <frag1337n@gmail.com>
2025-04-07 19:59:51 +0300

    Some reactoring

--- ulang/src/compiler/src/errors/diagnostic.rs
+++ ulang/src/compiler/src/errors/diagnostic.rs
@@ -4,17 +4,52 @@ use crate::span::Span;
 
 use super::emmiter::DynEmitter;
 
-pub struct Diagnostic<'a> {
-    diag_ctxt: &'a DiagnosticCtxt,
+pub struct Diagnostic<'dcx, 'msg> {
+    diag_ctxt: &'dcx DiagnosticCtxt,
     level: DiagnosticLevel,
-    diag_msg: DiagnosticMsg<'a>,
+    diag_msg: DiagnosticMsg<'msg>,
     span: Span,
 }
 
+pub struct DiagnosticCtxt {
+    emitter: Box<DynEmitter>,
+    into_console: bool,
+    into_file: bool,
+    allow_warns: bool,
+}
+
+#[derive(Clone, Copy)]
 pub enum DiagnosticLevel {
     Help,
     Warn,
     Error,
+}
+
+#[derive(Debug, Clone)]
+pub struct DiagnosticMsg<'s> {
+    msg: Cow<'s, str>,
+}
+
+/// A trait that allows you to easily create your own errors and
+/// parse them into diagnostics that are used throughout the compiler.
+/// To make it easier to create these errors, a [`derive-macro`](https://doc.rust-lang.org/reference/procedural-macros.html#derive-macros)
+/// has been implemented, which is located in the `ulang-macros` crate.
+pub trait IntoDiagnostic<'dcx, 'msg> {
+    fn into_diag(
+        &self,
+        diag_ctxt: &'dcx DiagnosticCtxt,
+        level: DiagnosticLevel,
+    ) -> Diagnostic<'dcx, 'msg>;
+}
+
+pub trait IntoDiagMsg {
+    fn into_diagnostic_msg<'s>(self) -> DiagnosticMsg<'s>;
+}
+
+impl IntoDiagMsg for String {
+    fn into_diagnostic_msg<'s>(self) -> DiagnosticMsg<'s> {
+        DiagnosticMsg { msg: self.into() }
+    }
 }
 
 impl ToString for DiagnosticLevel {
@@ -27,39 +62,56 @@ impl ToString for DiagnosticLevel {
     }
 }
 
-pub struct DiagnosticCtxt {
-    emitter: Box<DynEmitter>,
+impl<'dcx, 'msg> Diagnostic<'dcx, 'msg> {
+    pub fn new(
+        diag_ctxt: &'dcx DiagnosticCtxt,
+        level: DiagnosticLevel,
+        diag_msg: impl IntoDiagMsg,
+        span: Span,
+    ) -> Self {
+        Self {
+            diag_ctxt,
+            level,
+            diag_msg: diag_msg.into_diagnostic_msg(),
+            span,
+        }
+    }
 }
 
-impl DiagnosticCtxt {
-    pub fn new(emitter: Box<DynEmitter>) -> Self {
-        Self { emitter }
+impl<'dcx, 'msg> DiagnosticCtxt {
+    pub fn new(
+        emitter: Box<DynEmitter>,
+        into_console: bool,
+        into_file: bool,
+        allow_warns: bool,
+    ) -> Self {
+        Self {
+            emitter,
+            into_console,
+            into_file,
+            allow_warns,
+        }
     }
 
-    pub fn emit_err<'a>(&'a self, err: impl IntoDiagnostic<'a>) -> Diagnostic<'a> {
-        let err = self.struct_err(err);
+    pub fn emit_err(
+        &'dcx self,
+        err: impl IntoDiagnostic<'dcx, 'msg>,
+        level: DiagnosticLevel,
+    ) -> Diagnostic<'dcx, 'msg> {
+        let err = self.struct_err(err, level);
         self.emit(&err);
         err
     }
 
-    pub fn struct_err<'a>(&'a self, err: impl IntoDiagnostic<'a>) -> Diagnostic<'a> {
-        err.into_diag(self)
+    pub fn struct_err(
+        &'dcx self,
+        err: impl IntoDiagnostic<'dcx, 'msg>,
+        level: DiagnosticLevel,
+    ) -> Diagnostic<'dcx, 'msg> {
+        err.into_diag(self, level)
     }
 
     pub fn emit(&self, diag: &Diagnostic) {
         self.emitter.emit_diag(&diag);
     }
-}
-
-#[derive(Debug, Clone)]
-pub struct DiagnosticMsg<'s> {
-    msg: Cow<'s, str>,
-}
-
-/// A trait that allows you to easily create your own errors and
-/// parse them into diagnostics that are used throughout the compiler.
-/// To make it easier to create these errors, a [`derive-macro`](https://doc.rust-lang.org/reference/procedural-macros.html#derive-macros)
-/// has been implemented, which is located in the `ulang-macros` crate.
-pub trait IntoDiagnostic<'d> {
-    fn into_diag(&self, diag_ctxt: &'d DiagnosticCtxt) -> Diagnostic<'d>;
 }
--- ulang/src/compiler/src/errors/emmiter.rs
+++ ulang/src/compiler/src/errors/emmiter.rs
@@ -5,10 +5,3 @@ pub type DynEmitter = dyn Emitter;
 pub trait Emitter: std::fmt::Debug {
     fn emit_diag(&self, diag_msg: &Diagnostic);
 }
-
-/// `NOT IMPLEMENTED YET!`
-pub struct OutpEmitter<'d> {
-    diags: Vec<Diagnostic<'d>>,
-    into_console: bool,
-    into_file: bool,
-}
