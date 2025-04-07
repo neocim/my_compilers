df1defdf43d27e81b6297b90eebedd42b3c3407d
neocim <frag1337n@gmail.com>
2025-04-07 23:20:05 +0300

    Oops

--- ulang/src/compiler/src/lib.rs
+++ ulang/src/compiler/src/lib.rs
@@ -1,13 +1,5 @@
-use macros::IntoDiagnostic;
-
 pub mod ast;
 pub mod errors;
 pub mod lexer;
 pub mod span;
 pub mod symbol;
-
-#[derive(IntoDiagnostic)]
-#[message("Maxim gay {sfasf}")]
-struct Pipiska {
-    sfasf: String,
-}
